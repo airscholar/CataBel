dbab0d1f6be8645a8cf374f1a6c70efb57031376
Yusuf Ganiyu <airscholar@gmail.com>
2021-10-09 17:42:56 +0100

    Added seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,24 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+BeerCatalog.Repo.insert!(%BeerCatalog.Catalog.Beer{
+  brand: "Milwaukee's Best Light",
+  style: "American-Style Light Lager",
+  origin: "USA",
+  quantity: 1200
+})
+
+BeerCatalog.Repo.insert!(%BeerCatalog.Catalog.Beer{
+  brand: "Dos Equis",
+  style: "Lager Especial",
+  origin: "Mexico",
+  quantity: 430
+})
+
+BeerCatalog.Repo.insert!(%BeerCatalog.Catalog.Beer{
+  brand: "Maibock",
+  style: "Bavarian lager",
+  origin: "Germany",
+  quantity: 4000
+})
